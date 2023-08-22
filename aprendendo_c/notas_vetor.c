7b0ae319c4493581357ed7d0e99f21331ae68e02
pablitohaddad <pablohaddad73@gmail.com>
2023-08-22 10:27:24 -0400

    medias usando vetores

--- aprendendo_c/notas_vetor.c
+++ aprendendo_c/notas_vetor.c
@@ -3,19 +3,21 @@
 
 int main(){
 
-    float notas[4];
+    float notas[5], media, soma_notas;
 
     for(int i = 0; i < sizeof(notas)/sizeof(float); i++){
 
         printf("Digite a nota %d: ", i+1);
         scanf("%f", &notas[i]);
 
+        soma_notas += notas[i];
+
     }
     for(int i = 0; i < sizeof(notas)/sizeof(float); i++){
 
         printf("Nota [%u] = %.2f\n", i + 1, notas[i]);
-    }
-    
-    
 
+    }
+    media = soma_notas / (sizeof(notas)/sizeof(float));
+    printf("Media das notas: %.2f", media);
 }
