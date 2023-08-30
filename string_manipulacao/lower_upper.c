8c22372ce793dc8cb4f6f804816c554d60c5ccbe
pablitohaddad <pablohaddad73@gmail.com>
2023-08-30 10:30:00 -0400

    to lower e to upper

---
+++ string_manipulacao/lower_upper.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include  <string.h> 
+#include <ctype.h>      /*para tolower e toupper*/
+
+int main()
+{
+   printf("tolower(A) = %c\n", tolower('A'));
+   printf("toupper(a) = %c", toupper('a'));
+
+}
